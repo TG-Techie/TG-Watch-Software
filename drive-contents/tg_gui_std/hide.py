3dd91eae432960d368fefda09a3cd8f887136293
Jonah Yolles-Murphy <TGTechie01@gmial.com>
2021-03-08 12:45:20 -0500

    fix std.hide

--- drive-contents/tg_gui_std/hide.py
+++ drive-contents/tg_gui_std/hide.py
@@ -19,7 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-from tg_gui_core.base import Container, Widget
+from tg_gui_core.base import Container, Widget, center
 from tg_gui_core.stateful import State
 
 
