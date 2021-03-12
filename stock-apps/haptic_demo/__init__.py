8ce7858fadc6212048e92878b21be844ccb12b18
electricalhog <electricalhog10@gmail.com>
2021-03-12 12:01:53 -0500

    update demo with new internals features

--- stock-apps/haptic_demo/__init__.py
+++ stock-apps/haptic_demo/__init__.py
@@ -1,4 +1,4 @@
-from tg_gui_platform.all import *
+from tg_gui_std.all import *
 from system.applocals import *
 from hardware import drivers
 import adafruit_drv2605
@@ -10,14 +10,20 @@ class Application(Layout):
         drivers.haptic.sequence[0] = adafruit_drv2605.Effect(mode)
         drivers.haptic.play()
 
-    but1 = Button(text="Click", press=self.vibrate(1))
-    but2 = Button(text="Sharp", press=self.vibrate(4))
-    but3 = Button(text="Soft", press=self.vibrate(7))
-    but4 = Button(text="Buzz", press=self.vibrate(14))
+    buttons = VSplit(
+        HSplit(
+            Button(text="Click", press=self.vibrate(1)),
+            Button(text="Sharp", press=self.vibrate(4)),
+        ),
+        HSplit(
+            Button(text="Soft", press=self.vibrate(7)),
+            Button(text="Buzz", press=self.vibrate(14)),
+        ),
+        HSplit(
+            Button(text="Double", press=self.vibrate(10)),
+            Button(text="Alert", press=self.vibrate(16)),
+        ),
+    )
 
     def _any_(self):
-        size = (self.width // 2, self.height // 2)
-        b1 = self.but1((left, top), size)
-        b2 = self.but2((right, top), size)
-        b3 = self.but3((left, bottom), size)
-        b4 = self.but4((right, bottom), size)
+        self.buttons(center, (self.width, self.height))
