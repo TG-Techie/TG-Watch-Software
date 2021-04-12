4d3cda69bdb48c0bc0060feff91507469178b2b2
Jonah Yolles-Murphy <TGTechie01@gmial.com>
2021-04-12 00:05:11 -0400

    add low bat splash (needs fleshing out)

---
+++ drive-contents/setup/low_battery_splash.py
@@ -0,0 +1,24 @@
+import gc
+import time
+import displayio
+
+from hardware import display
+
+from adafruit_display_text import label
+
+# from adafruit_progressbar import ProgressBar
+import terminalio
+
+splash = displayio.Group()
+display.show(splash)
+
+splash_text = label.Label(
+    terminalio.FONT, text="Low Battery", color=0xFFFFFF, scale=3, x=25, y=80
+)
+
+# progress_bar = ProgressBar(30, 120, 180, 7, progress=0.0, stroke=0, bar_color=0xFFFFFF)
+
+splash.append(splash_text)
+time.sleep(0.05)
+display.refresh()
+splash = None
